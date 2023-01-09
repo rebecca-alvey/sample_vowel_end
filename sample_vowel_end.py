92f5a121b408d1de923b5a972c00a7164e3445e7
rebecca-alvey <rebecca.j.alvey@gmail.com>
2023-01-09 13:25:18 -0700

    Create a function to perform the test. Update variable to test from i for input to w for word.

--- sample_vowel_end.py
+++ sample_vowel_end.py
@@ -4,16 +4,21 @@ import re
 import sys
 import argparse
 
+#function to test if the word is a vowel or not, returns bool
+def testWordEndVowel(word):
+    vowel = re.search('[aeiou]\Z', word, re.IGNORECASE)
+    return vowel
+
 parser = argparse.ArgumentParser();
 parser.add_argument('word_to_test');
 
 #argparse will throw useage error here if the input is invalid, no need to test sys.argv length
 args = parser.parse_args();
 
-
-i = args.word_to_test;
-v = re.search('[aeiou]\Z', i, re.IGNORECASE)
+w = args.word_to_test;
+v = testWordEndVowel(w)
 if v:
-    print(i + ' is a word that ends in a vowel.')
+    print(w + ' is a word that ends in a vowel.')
 else:
-    print(i + ' is not a word that ends in a vowel.')
+    print(w + ' is not a word that ends in a vowel.')
+
