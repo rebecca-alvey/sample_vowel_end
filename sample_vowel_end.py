ffe72d4c8c90d638af631d476ae9a74584c7520d
rebecca-alvey <rebecca.j.alvey@gmail.com>
2023-01-09 13:15:16 -0700

    Update to use argparse instead of sys.argv

--- sample_vowel_end.py
+++ sample_vowel_end.py
@@ -2,13 +2,18 @@
 
 import re
 import sys
+import argparse
 
-if len(sys.argv) == 2:
-    i = sys.argv[1]
-    v = re.search('[aeiou]\Z', i, re.IGNORECASE)
-    if v:
-        print(i + ' is a word that ends in a vowel.')
-    else:
-        print(i + ' is not a word that ends in a vowel.')
+parser = argparse.ArgumentParser();
+parser.add_argument('word_to_test');
+
+#argparse will throw useage error here if the input is invalid, no need to test sys.argv length
+args = parser.parse_args();
+
+
+i = args.word_to_test;
+v = re.search('[aeiou]\Z', i, re.IGNORECASE)
+if v:
+    print(i + ' is a word that ends in a vowel.')
 else:
-    print('Usage: sample_vowel_end.py word_to_test')
+    print(i + ' is not a word that ends in a vowel.')
